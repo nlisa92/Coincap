552ef2f6e1e2716464a92ab8562a8e61e2e8ad3a
nlisa92 <irina.zamula.92@gmail.com>
2025-12-28 00:24:04 +0300

    feat: add CryptoTable component

---
+++ src/components/MainPage/CryptoTable.jsx
@@ -0,0 +1,23 @@
+import CryptoRow from "./CryptoRow";
+
+const CryptoTable = ({ currencies, onAdd }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Change</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {currencies.map((currency) => (
+          <CryptoRow key={currency.id} onAdd={onAdd} currency={currency} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+export default CryptoTable;
