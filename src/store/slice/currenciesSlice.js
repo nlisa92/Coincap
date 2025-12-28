d1c617f20eaac6eedcbfb5f2c0827ec4314f9ae6
nlisa92 <irina.zamula.92@gmail.com>
2025-12-28 18:58:33 +0300

    feat: implement currencies slice with async fetch

---
+++ src/store/slice/currenciesSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchCurrencies = createAsyncThunk(
+    "currencies/fetchCurrencies",
+    async () => {
+        const res = await fetch("https://api.coincap.io/v2/assets");
+        const data = await res.json();
+        return data.data;
+    }
+)
+const currenciesSlice = createSlice({
+    name: "currencies",
+    initialState: {
+        list: [],
+        status: "idle",
+        error: null
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchCurrencies.pending, (state) => {
+            state.status = 'loading';
+        })
+        .addCase(fetchCurrencies.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.list = action.payload;
+        })
+        .addCase(fetchCurrencies.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+        })
+    }
+});
+export default currenciesSlice.reducer;
