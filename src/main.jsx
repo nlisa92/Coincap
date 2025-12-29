eb528873f7e14f7bbf8abbcc10bdd2338e99e9c9
nlisa92 <irina.zamula.92@gmail.com>
2025-12-29 16:08:28 +0300

    feat: add HashRouter to main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { HashRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </StrictMode>
+);
