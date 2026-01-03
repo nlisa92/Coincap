c3f5b06f129817442a044e720e6a352b4895aa19
nlisa92 <irina.zamula.92@gmail.com>
2026-01-03 21:54:28 +0300

    feat: refactor CryptoRow currency formattin

--- src/components/MainPage/CryptoRow.jsx
+++ src/components/MainPage/CryptoRow.jsx
@@ -1,16 +1,34 @@
 import ButtonAdd from "../Buttons/ButtonAdd";
 
 const CryptoRow = ({ currency, onAdd }) => {
-  const { rank, name, symbol, marketCapUsd, priceUsd, changePercent24Hr } =
-    currency;
+  const {
+    rank,
+    name,
+    symbol,
+    marketCapUsd,
+    priceUsd,
+    changePercent24Hr,
+    vwap24Hr,
+  } = currency;
+
+  const formatCurrency = (value) =>
+    value !== "null" && value !== "undefined"
+      ? Number(value).toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          maximumFractionDigits: 2,
+        })
+      : "—";
+
   return (
     <tr>
       <td>{rank}</td>
       <td>
         {name} ({symbol})
       </td>
-      <td>{Number(marketCapUsd).toFixed(2)}</td>
-      <td>{Number(priceUsd).toFixed(2)} USD</td>
+      <td>{formatCurrency(vwap24Hr)}</td>
+      <td>{formatCurrency(marketCapUsd)}</td>
+      <td>{formatCurrency(priceUsd)}</td>
       <td>{Number(changePercent24Hr).toFixed(2)}%</td>
       <td>
         <ButtonAdd onClick={onAdd} />
