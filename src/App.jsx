807b4e718af5c9f7962917c812c842b06d3de56a
nlisa92 <irina.zamula.92@gmail.com>
2026-01-05 14:55:56 +0300

    fix: render Header outside Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </>
   );
 }
 
