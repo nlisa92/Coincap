fc25b59adbeb75f19e697355c1e8a8f3686bf7ee
nlisa92 <irina.zamula.92@gmail.com>
2026-01-10 15:11:38 +0300

    feat: add styles to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,33 +8,57 @@ const Header = ({ onOpenPortfolio }) => {
   const { list } = useSelector((state) => state.currencies);
   const { total, diff, diffPercent } = useSelector((state) => state.portfolio);
   const popularCurrencies = list.filter((item) => popular.includes(item.id));
+
   return (
-    <div>
-      <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "8px 0",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          gap: 16,
+        }}
+      >
         {popularCurrencies.map((item) => (
-          <div key={item.id}>
+          <div
+            key={item.id}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
             <span>{item.name}</span>
             <span>{formatCurrency(item.priceUsd)}</span>
           </div>
         ))}
       </div>
-      <div onClick={onOpenPortfolio}>
+
+      <div
+        onClick={onOpenPortfolio}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          cursor: "pointer",
+          gap: 8,
+        }}
+      >
         <WalletOutlined />
 
         <span>Итого: {formatCurrency(total)}</span>
 
         {total > 0 && (
           <>
-            <span style={{ color: diff >= 0 ? "green" : "red", marginLeft: 8 }}>
-              {diff >= 0 ? "+" : ""} {formatCurrency(diff)}
+            <span style={{ color: diff >= 0 ? "green" : "red" }}>
+              {diff >= 0 ? "+" : ""}
+              {formatCurrency(diff)}
             </span>
 
-            <span
-              style={{
-                color: diffPercent >= 0 ? "green" : "red",
-                marginLeft: 8,
-              }}
-            >
+            <span style={{ color: diffPercent >= 0 ? "green" : "red" }}>
               {diffPercent >= 0 ? "+" : ""}
               {diffPercent.toFixed(2)}%
             </span>
