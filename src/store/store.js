e368e34f6804b8197ead9bbf80e9e93eafeba476
nlisa92 <irina.zamula.92@gmail.com>
2026-01-17 18:16:46 +0300

    feat: add chartSlice reducer to Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import currenciesReducer from "./slice/currenciesSlice"; 
+import currenciesReducer from "./slice/currenciesSlice";
 import portfolioReducer from "./slice/portfolioSlice";
+import chartReducer from "./slice/chartSlice";
 
 export const store = configureStore({
   reducer: {
-    currencies: currenciesReducer, 
+    currencies: currenciesReducer,
     portfolio: portfolioReducer,
+    chart: chartReducer,
   },
 });
