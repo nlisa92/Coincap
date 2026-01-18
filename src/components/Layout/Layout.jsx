a394dbe484ea8b1e694bbdaea5cedea083985df2
nlisa92 <irina.zamula.92@gmail.com>
2026-01-18 22:27:57 +0300

    refactor: move portfolio modal logic to Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { removeCurrency } from "../../store/slice/portfolioSlice";
 import Header from "../Header/Header";
+import PortfolioModal from "../portfolioModal/PortfolioModal";
 
 const Layout = () => {
+  const dispatch = useDispatch();
+  const { items } = useSelector((state) => state.portfolio);
+
+  const [isPortfolioOpen, setIsPortfolioOpen] = useState(false);
+
   return (
     <>
-      <Header />
+      <Header onOpenPortfolio={() => setIsPortfolioOpen(true)} />
       <main>
         <Outlet />
       </main>
+      <PortfolioModal
+        open={isPortfolioOpen}
+        items={items}
+        onDelete={(id) => dispatch(removeCurrency(id))}
+        onClose={() => setIsPortfolioOpen(false)}
+      />
     </>
   );
 };
