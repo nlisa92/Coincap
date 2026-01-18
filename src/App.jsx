8db96d971575e1cf22080d5d6a18fc65ac8a58dc
nlisa92 <irina.zamula.92@gmail.com>
2026-01-18 21:33:46 +0300

    refactor: wrap routes with Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import CurrencyPage from "./components/currencyPage/CurrencyPage";
+import Layout from "./components/Layout/Layout";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<MainPage />} />
         <Route path="/currency/:id" element={<CurrencyPage />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 }
 
